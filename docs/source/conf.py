db4ad188b5d8f535383dc9fc478976b1cccfb736
Arpita <59466546+Arpita2512@users.noreply.github.com>
2023-05-05 01:22:48 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx.ext.autosummary'
+    'sphinx.ext.autosummary',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
